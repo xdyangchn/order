ff4086ac04ce882200d8dcd28c65f1d36432cc37
yangxd <709820562@qq.com>
2018-06-26 18:19:48 +0800

    update

--- server/src/main/java/com/spring/yxd/order/OrderApplication.java
+++ server/src/main/java/com/spring/yxd/order/OrderApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.spring.yxd.product.client")
---
+++ server/src/main/java/com/spring/yxd/order/controller/EnvController.java
@@ -0,0 +1,23 @@
+package com.spring.yxd.order.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 杨贤达
+ * 2018/6/26 12:33
+ */
+@RestController
+@RequestMapping("/env")
+public class EnvController {
+
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/print")
+    public String print() {
+        return env;
+    }
+}
